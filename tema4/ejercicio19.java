50fede64abee8fba298939d3d3abc2030ec1dd35
sergioalmagrorojas <sergiolmgr92@gmail.com>
2019-10-02 14:03:50 +0200

    commit

--- tema4/ejercicio19.java
+++ tema4/ejercicio19.java
@@ -14,8 +14,29 @@ public class ejercicio19 {
     public static void main(String []   args){
         Scanner s =new Scanner(System.in);
         
+        int numero;
+        int digito=0;
+        System.out.println("Introduzca un entero y pulse intro");
+        numero=Integer.parseInt(s.nextLine());
         
         
+        if(numero<10){
+            digito=1;
+        }
+        if((numero>10)&&(numero<100)){
+            digito=2;
+        }
+        if((numero>100)&&(numero<1000)){
+            digito=3;
+        }
+        if((numero>1000)&&(numero<10000)){
+            digito=4;
+        }
+        if((numero>10000)&&(numero<100000)){
+            digito=5;
+        }
+        
+        System.out.println("El número " + numero +" tiene : " + digito + " digito/s");
         
         
     }
